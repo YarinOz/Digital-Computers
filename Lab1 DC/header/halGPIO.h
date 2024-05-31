89fa3f99cee4688383b0c25dabcd1314185edfbb
Yarin Oziel <139881999+YarinOz@users.noreply.github.com>
2024-05-31 13:39:25 +0300

    Update halGPIO.h

--- Lab1 DC/header/halGPIO.h
+++ Lab1 DC/header/halGPIO.h
@@ -1,7 +1,7 @@
 #ifndef _halGPIO_H_
 #define _halGPIO_H_
 
-#ifdef _MSP430FG4619_
+#ifdef __MSP430FG4619__
 #include "../header/bsp_msp430x4xx.h" //lab kit
 #else
 #include "../header/bsp_msp430x2xx.h" //pesronal kit
