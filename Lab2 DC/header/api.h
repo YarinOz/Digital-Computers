ee7be910234afc0a5581706c21a75425454a5db3
Yarin Oziel <139881999+YarinOz@users.noreply.github.com>
2024-06-09 14:18:52 +0300

    Add files via upload

---
+++ Lab2 DC/header/api.h
@@ -0,0 +1,12 @@
+#ifndef _api_H_
+#define _api_H_
+
+#include  "../header/halGPIO.h"     // private library - HAL layer
+
+extern void CountDown();
+extern void startTimerA0();
+extern void freqMeas();
+extern void tone_generator();
+extern void Signal_shape();
+
+#endif
