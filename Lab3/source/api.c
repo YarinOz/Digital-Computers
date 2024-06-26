615725cc78752d9aa8cc2dad93f61c8e99b634b6
YarinOz <oziely@post.bgu.ac.il>
2024-06-26 16:58:28 +0300

    update

--- Lab3/source/api.c
+++ Lab3/source/api.c
@@ -54,7 +54,7 @@ void IdiomRecorder(){
 void Merge() {
     char merge1[51], merge2[51];
     char temp1[51], temp2[51];
-    int len1, len2;
+    int len1, len2, sent1len, sent2len;
     char *ptr1, *ptr2, *ptr_merge, *print;
     char *end1, *end2;
     char *space_pos;
@@ -63,6 +63,8 @@ void Merge() {
     lcd_home();
     lcd_puts("First index: ");
     lcd_goto(0x40);
+    delay(1500); // debounce
+    delay(1500);
     delay(1500);
     delay(1500);
     __bis_SR_register(LPM0_bits + GIE);
@@ -72,6 +74,8 @@ void Merge() {
     lcd_home();
     lcd_puts("Second index: ");
     lcd_goto(0x40);
+    delay(1500); // debounce
+    delay(1500);
     delay(1500);
     delay(1500);
     __bis_SR_register(LPM0_bits + GIE);
@@ -83,8 +87,10 @@ void Merge() {
     ptr1 = merge1;
     ptr2 = merge2;
     ptr_merge = strMerge;
-    end1 = merge1 + strlen(merge1);
-    end2 = merge2 + strlen(merge2);
+    sent1len = strlen(merge1);
+    sent2len = strlen(merge2);
+    end1 = merge1 + sent1len;
+    end2 = merge2 + sent2len;
     strMerge[0] = '\0'; // init the string
 
     while (ptr1 < end1 || ptr2 < end2) {
@@ -92,9 +98,12 @@ void Merge() {
         if (ptr1 < end1) {
             space_pos = strchr(ptr1, ' ');
             if (space_pos) {
-                len1 = space_pos - ptr1;
+                len1 = space_pos - ptr1 + 1;
             } else {
                 len1 = end1 - ptr1;
+                if (ptr1 + len1 < end1) {
+                    len1 += 1; // Include space if it's not the last word
+                }
             }
 
             // Use DMA to transfer the word
@@ -107,24 +116,18 @@ void Merge() {
 
             ptr1 += len1;
             ptr_merge += len1;
-
-            if (*ptr1 == ' ') {
-                // *ptr_merge = ' ';
-                ptr1++;
-                ptr_merge++;
-            } else {
-                *ptr_merge = ' ';
-                ptr_merge++;
-            }
         }
 
         // Find the next word in merge2
         if (ptr2 < end2) {
             space_pos = strchr(ptr2, ' ');
             if (space_pos) {
-                len2 = space_pos - ptr2;
+                len2 = space_pos - ptr2 + 1;
             } else {
                 len2 = end2 - ptr2;
+                if (ptr2 + len2 < end2) {
+                    len2 += 1; // Include space if it's not the last word
+                }
             }
 
             // Use DMA to transfer the word
@@ -138,14 +141,6 @@ void Merge() {
             ptr2 += len2;
             ptr_merge += len2;
 
-            if (*ptr2 == ' ') {
-                // *ptr_merge = ' ';
-                ptr2++;
-                ptr_merge++;
-            } else {
-                *ptr_merge = ' ';
-                ptr_merge++;
-            }
         }
     }
 
