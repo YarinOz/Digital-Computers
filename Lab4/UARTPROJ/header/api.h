e0258fbfc0cfc4ef2bcf489731c5028157e07097
Yarin Oziel <139881999+YarinOz@users.noreply.github.com>
2024-07-11 15:50:29 +0300

    Add files via upload

---
+++ Lab4/UARTPROJ/header/api.h
@@ -0,0 +1,23 @@
+#ifndef _api_H_
+#define _api_H_
+
+#include  "../header/halGPIO.h"     // private library - HAL layer
+
+
+extern void blinkRGB();
+extern void count_up_LCD();
+extern void count_down_LCD();
+extern void clear_counters();
+extern void measure_pot();
+extern void change_delay_time();
+
+
+
+#endif
+
+
+
+
+
+
+
