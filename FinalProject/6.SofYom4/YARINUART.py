7baa34f681423db7b5db2b64124461dfcb0705d6
YarinOz <oziely@post.bgu.ac.il>
2024-08-30 08:44:37 +0300

    \n script

--- FinalProject/6.SofYom4/YARINUART.py
+++ FinalProject/6.SofYom4/YARINUART.py
@@ -241,6 +241,8 @@ class ScriptMode:
         self.translated_text.grid(row=2, column=2, padx=10, pady=10, sticky='nsew')
 
         # Flashed Files Listbox (Execution Listbox)
+        self.translated_text_label = ttk.Label(self.top, text="Execute Script")
+        self.translated_text_label.grid(row=1, column=3, padx=10, pady=5, sticky='w')
         self.execution_listbox = tk.Listbox(self.top, width=30, height=15, selectmode=tk.SINGLE)
         self.execution_listbox.grid(row=2, column=3, padx=10, pady=10, rowspan=1, sticky='ns')
 
@@ -249,14 +251,14 @@ class ScriptMode:
         self.select_button.grid(row=3, column=0, padx=10, pady=10, sticky='ew')
 
         self.load_button = ttk.Button(self.top, text="Flash File", command=self.load_file)
-        self.load_button.grid(row=3, column=1, padx=10, pady=10, sticky='ew')
+        self.load_button.grid(row=3, column=1, columnspan=4,padx=10, pady=10, sticky='ew')
 
         self.execute_button = ttk.Button(self.top, text="Execute Script", command=self.execute_script)
         self.execute_button.grid(row=3, column=3, padx=10, pady=10, sticky='ew')
 
         # Add a "Back" button
         self.back_button = ttk.Button(self.top, text="Back", command=self.close_script_mode)
-        self.back_button.grid(row=4, column=0, columnspan=4, padx=10, pady=10, sticky='ew')
+        self.back_button.grid(row=4, column=0, columnspan=2, padx=10, pady=10, sticky='ew')
 
         self.files = []  # List to store selected file paths
         self.translated_contents = [None, None, None]  # To store translated content for each file
@@ -321,7 +323,7 @@ class ScriptMode:
                 print(f"Error translating file: {e}")
                 return
             
-            translated_content = bytes(self.translated_contents[index] + 'Z', 'utf-8')  # Append 'Z' end marker
+            translated_content = bytes('\n' + self.translated_contents[index] + 'Z', 'utf-8')  # Append 'Z' end marker
             # Send the translated content using execute_serial_command
             print(f"Flashing translated script from {os.path.basename(file_path)}:\n\n{translated_content}")
             self.execute_serial_command(translated_content, file=True)
